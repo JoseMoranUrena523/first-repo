7b33fcc7d945b36da65825242b7d38df5679af4f
Jose Moran Urena <jose@josemoranurena.tech>
2023-09-21 16:01:00 -0400

    second to final commit

--- workspace/littlelemon/littlelemon/urls.py
+++ workspace/littlelemon/littlelemon/urls.py
@@ -13,6 +13,7 @@ urlpatterns = [
     path('restaurant/', include('restaurant.urls')),
     path('restaurant/menu/', include('restaurant.urls')),
     path('restaurant/booking/', include(router.urls)),
+    path('api/bookings/', include(router.urls)),
     path('auth/', include('djoser.urls')),
     path('auth/', include('djoser.urls.authtoken')),
     path('api-token-auth/', obtain_auth_token),
